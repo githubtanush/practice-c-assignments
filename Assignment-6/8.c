a7dfb9986bac9eaf8ddda7ac542bc237c6baca27
TANUSH ARORA <tanusharora@192.168.1.2>
2023-06-13 13:00:47 +0530

    fifth commit'

--- Assignment-6/8.c
+++ Assignment-6/8.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main(){
-    int a,b,c;
-    printf("Enter the sides of a triangle");
-    scanf("%d %d %d",&a,&b,&c);
-    if(a+b>c && b+c>a && c+a>b){
-        printf("triangle is valid\n");
+    int a;
+    printf("Enter the number of the month: ");
+    scanf("%d",&a);
+    if(a==1||a==3||a==5||a==7||a==8||a==10||a==12){
+        printf("The number of days in this month is 31\n");
+    }
+    else if(a==4||a==6||a==9||a==11){
+        printf("The number of days in this month is 30\n");
     }
     else{
-        printf("Triangle is not valild\n");
+        printf("The no. of days in this month is 28\n");
     }
 }
