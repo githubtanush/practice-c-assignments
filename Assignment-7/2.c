27f8c49a383c2c018cd03e90c036e33debef5098
TANUSH ARORA <tanusharora@192.168.1.2>
2023-06-13 16:12:53 +0530

    eight commit

--- Assignment-7/2.c
+++ Assignment-7/2.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(){
-    int i=1;
-    while(i<=10){
+    int i=1,n;
+    printf("Enter the number for n which do you want till counting");
+    scanf("%d",&n);
+    while(i<=n){
         printf("%d\n",i);
         i++;
     }
