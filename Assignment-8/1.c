529e6cf219faa9723b0910fb936e5a9c6d33cd7c
TANUSH ARORA <tanusharora@192.168.1.2>
2023-06-13 16:29:54 +0530

    ninth commit

---
+++ Assignment-8/1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("Enter the n till what u want till the counting");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        printf("MySirG\n");
+    }
+    getchar();
+}
---
+++ Assignment-8/10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("Enter the n till what u want till the counting: ");
+    scanf("%d",&n);
+    for(i=10;i>=n;i--){
+        printf("%d\n",i*i*i);
+    }
+    getchar();
+}
